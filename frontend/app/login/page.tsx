b65d6e0380aa340c55e065b39aef52e5f35c466e
G1119 <jasonzhong19941119@gmail.com>
2023-11-21 20:34:58 -0800

    Login Ui done

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react'
 
 // assets
@@ -8,39 +10,36 @@ import { FcGoogle } from "react-icons/fc";
 import { FaApple } from "react-icons/fa";
 
 function Login () {
+
   return (
-    <div className='w-full h-screen bg-amber-500'>
-      <div className='flex text-center items-center px-20 py-12'>
-        <img
-          className='w-[50%] align-middle'
-          src={CarStation.src}
-        />
+    <div className='w-full h-fit bg-amber-500 pt-7'>
+      <div className='block lg:flex text-center items-center px-10 md:px-20 pt-7 pb-16'>
         {/* Login Form */}
-        <div className='w-[40%] h-[500px] m-auto bg-neutral-900 p-6 rounded-lg block'>
+        <div className='w-[100%] md:w-[70%] lg:w-[40%] h-fit m-auto bg-neutral-900 p-6 rounded-lg block'>
           <h1 className='text-left text-2xl font-bold text-gray-100'>Login to your Account</h1>
           <div className='px-1'>
-            <div className='w-full flex justify-between mt-5'>
+            <div className='w-full block lg:flex justify-between mt-5'>
               <a
                 href='#'
-                className='w-[48%] h-10 flex items-center justify-evenly px-2 text-gray-300 border-gray-200 hover:bg-neutral-800 hover:text-white hover:scale-105 border-x border-y rounded-xl'
+                className='w-full lg:w-[48%] mb-5 md h-10 flex items-center gap-1 justify-center px-2 text-gray-300 border-gray-200 hover:bg-neutral-800 hover:text-white hover:scale-105 border-x border-y rounded-xl'
               >
                 <FcGoogle />
-                <p className='text-sm font-bold font-opensans mb-0'>Log in with Google</p>
+                <p className='xl:text-sm text-xs font-bold font-opensans mb-0'>Log in with Google</p>
               </a>
               <a
                 href='#'
-                className='w-[48%] h-10 flex items-center justify-evenly px-2 text-gray-300 border-gray-200 hover:bg-neutral-800 hover:text-white hover:scale-105 border-x border-y rounded-xl'
+                className='w-full lg:w-[48%] h-10 flex items-center gap-1 justify-center px-2 text-gray-300 border-gray-200 hover:bg-neutral-800 hover:text-white hover:scale-105 border-x border-y rounded-xl'
               >
                 <FaApple />
-                <p className='text-sm font-bold font-opensans mb-0'>Log in with Apple</p>
+                <p className='xl:text-sm text-xs font-bold font-opensans mb-0'>Log in with Apple</p>
               </a>
             </div>
-            <div className='w-full flex items-center my-5'>
+            <div className='w-full flex items-center my-4'>
               <div className='w-[45%] h-[1px] bg-gray-300'></div>
               <p className='px-4 text-sm text-gray-200'>or</p>
               <div className='w-[45%] h-[1px] bg-gray-300'></div>
             </div>
-            <h4 className='mt-2 text-left text-gray-100'>Email</h4>
+            <h4 className='mt-2 text-left text-gray-100 mt-0'>Email</h4>
             <input
               type="email"
               className='w-[99%] h-10 m-auto px-3 my-2 rounded-lg bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-700 focus:border-2'
@@ -58,19 +57,23 @@ function Login () {
                 <p className='text-gray-100 text-sm'>Show Password</p>
               </div>
               <a
-                className='text-amber-500 text-right text-sm'
+                className='text-amber-500 hover:text-amber-400 text-right text-sm'
                 href="#"
               >Forgot Password?</a>
             </div>
-            <button className='w-full h-10 mt-5 bg-indigo-700 text-gray-100 rounded-lg'>
+            <button className='w-full h-10 mt-5 bg-indigo-700 hover:bg-indigo-600 text-sm text-gray-100 rounded-lg'>
               Sign in to your Account
             </button>
             <div className='flex gap-2 text-left text-xs mt-5'>
               <p className='text-gray-300'>Don't have an account yet?</p>
-              <a className='text-amber-500' href="#">Sign up Here</a>
+              <a className='text-amber-500 hover:text-amber-400' href="#">Sign up Here</a>
             </div>
           </div>
         </div>
+        <img
+          className='w-full lg:w-[50%] align-middle mt-10'
+          src={CarStation.src}
+        />
       </div>
     </div>
   )
