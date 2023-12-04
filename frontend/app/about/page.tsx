ac5afcb641deb9dc3362d0fb2b0b49e1ff65269e
G1119 <jasonzhong19941119@gmail.com>
2023-12-03 20:54:45 -0800

    Working on About page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -5,6 +5,8 @@ import React from "react"
 
 // importing assets
 import AboutStation from '../assets/aboutStation.png'
+import ElectriPlanet from '../assets/ElectiPlanet.png'
+import Global from '../assets/global.png'
 
 // icons
 import { PiMagnifyingGlassDuotone } from "react-icons/pi";
@@ -15,7 +17,7 @@ function About() {
     return (
         <div className="w-full h-fit bg-amber-500 font-sans">
             {/* Hero Section */}
-            <div className="w-full min-h-screen px-20 flex items-center">
+            <div className="w-full px-20 flex items-center">
                 <div className="w-[40%] text-left mb-20">
                     <p className="text-black text-md italic mb-2">''The best Electric Search Network</p>
                     <h1 className="text-6xl italic text-black">Find your nearest Electric Charge Station</h1>
@@ -29,8 +31,26 @@ function About() {
                 </div>
             </div>
             {/* Sub title */}
-            <p className="w-[50%] text-center m-auto py-2 text-xl text-black font-semibold italic">Explore the seamless fusion of convenience and sustainability at the best Electric Stations Search Network. Your journey to eco-friendly electric vehicle charging starts here. Drive smart, drive green!</p>
-
+            <div className="bg-neutral-900 py-6">
+                <p className="w-[50%] text-center m-auto py-2 text-xl text-gray-100 font-semibol italic">Explore the seamless fusion of convenience and sustainability at the best Electric Stations Search Network. Your journey to eco-friendly electric vehicle charging starts here. Drive smart, drive green!</p>
+            </div>
+            {/* Solutions */}
+            <div className="w-full flex items-center text-center m-auto py-10">
+                <div className="w-[50%] text-lg font-semibold italic text-gray-800">
+                    <img
+                        src={Global.src}
+                        className="w-60 m-auto"
+                    />
+                    <h3>Search for Electric Charge Stations</h3>
+                </div>
+                <div className="w-[50%] text-lg font-semibold italic text-gray-800">
+                    <img
+                        src={ElectriPlanet.src}
+                        className="w-60 m-auto"
+                    />
+                    <h3>Making the World more green!</h3>
+                </div>
+            </div>
         </div>
     )
 }
