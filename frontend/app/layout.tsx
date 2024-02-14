f7f302630f380d46d9cdb440e3f144766a37f74c
G1119 <jasonzhong19941119@gmail.com>
2024-02-14 15:57:06 -0500

    Ts Type CrossOrigin fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children } : PropsWithChildren) => {
     <div>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&display=swap" rel="stylesheet"></link>
       </Head>
       <div className='w-full min-h-screen font-railway'>
