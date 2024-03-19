bd664ff22825ae5ad7854c10326ec66d5b06f8e8
G1119 <jasonzhong19941119@gmail.com>
2024-03-19 15:27:15 -0700

    Working on Login and Signup

--- backend/app/auth/__init__.py
+++ backend/app/auth/__init__.py
@@ -20,63 +20,56 @@ auth_blueprint = Blueprint('auth', __name__)
 # LOGIN ROUTE
 @auth_blueprint.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        # Get the form data
-        data = request.json
-        if data:
-            email = data["email"]
-            password = data['password']
-            
-            # Find User in the db
-            user = db.credentials.find_one({'email': email})
-            print(user)
-            
-            #check password
-            if user and check_password_hash(user['password'], password):
-                # Successful login
-                session['user'] = str(user['_id'])
-                return redirect(url_for('/'))
-            else:
-                flash("Invalid credentials")
+    # Get the form data
+    data = request.json
+    if data:
+        email = data["email"]
+        password = data['password']
         
+        # Find User in the db
+        user = db.credentials.find_one({'email': email})
+        print(user)
         
-        
-    return 'Hi there'
+        #check password
+        if user and check_password_hash(user['password'], password):
+            # Successful login
+            print('Good Password')
+            session['user'] = str(user['_id'])
+            return redirect(url_for('/'))
+        else:
+            flash("Invalid credentials")
+    
 
 
 
 
 # SIGNUP ROUTE
-@auth_blueprint.route('/signup', methods=['GET','POST'])
+@auth_blueprint.route('/signup', methods=['POST'])
 def signUp():
-    if request.method == 'POST':
-        # Get form data
-        email = request.form.get('email')
-        password = request.form.get('password')
-        verifyPassword = request.form.get('verifyPassword')
-        
-        # check if both passwords match
-        if password == verifyPassword:
-            # check if user already exists
-            user_exists = db.credentials.find_one({email:  email})
-            if user_exists:
-                flash('Username already exists.')
-                return redirect(url_for('/signup'))
-            
-            # if user doesn't exists hash password and create user
-            hashed_password = generate_password_hash(password, method='sha256')
-            
-            db.credentials.insert_one({
-                'email': email,
-                'password': hashed_password
-            })
-            
-            
-            flash('Account created successfully! Please login.')
-            return redirect(url_for('.login'))
+    # Get form data
+    email = request.form.get('email')
+    password = request.form.get('password')
+    verifyPassword = request.form.get('verifyPassword')
     
-    if request.method == 'GET':
-        return redirect(url_for('/signup'))
+    # check if both passwords match
+    if password == verifyPassword:
+        # check if user already exists
+        user_exists = db.credentials.find_one({email:  email})
+        if user_exists:
+            flash('Username already exists.')
+            return redirect(url_for('/signup'))
+        
+        # if user doesn't exists hash password and create user
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        db.credentials.insert_one({
+            'email': email,
+            'password': hashed_password
+        })
+        
+        
+        flash('Account created successfully! Please login.')
+        return redirect(url_for('.login'))
             
                 
 # LOGOUT ROUTE
