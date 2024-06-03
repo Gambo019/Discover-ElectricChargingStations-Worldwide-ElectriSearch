a14dd41810220753239b2306cb7bff7c23b5df9e
G1119 <jasonzhong19941119@gmail.com>
2024-06-03 14:15:01 -0700

    Updating Port number and APIs

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -16,7 +16,7 @@ function Login () {
 
   const handleLogin = async () => {
     // Make Post API call request to trigger backend login function
-    const response = await fetch('http://127.0.0.1:5000/login', {
+    const response = await fetch('http://127.0.0.1:4000/auth/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
