16eaa6e98de739e06e561f02c1fe961bcd8d8b34
G1119 <jasonzhong19941119@gmail.com>
2024-06-03 21:19:45 -0700

    Adding function connectToDB

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,28 +1,32 @@
 ////////////////////////////////
 // Database Connection MongoDb
 ////////////////////////////////
-const {MongoClient, ServerApiVersion} = require('mongodb')
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const URI = process.env.MONGO_URI
+const URI = process.env.MONGO_URI;
 
 const client = new MongoClient(URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
-        deprecationErros: true
-    }
-})
+        deprecationErrors: true,
+    },
+});
 
-try{
-    // Connect client to the server
-    await client.connect()
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-    console.log('Pinged your deployment. You have successfully connected to MongoDb!');
-}   catch (err){
-        console.log(`Error connecting to MongoDb: ${err}`)
+async function connectToDatabase() {
+    try {
+        // Connect client to the server
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You have successfully connected to MongoDb!');
+    } catch (err) {
+        console.log(`Error connecting to MongoDb: ${err}`);
+    }
 }
 
-let db = client.db('psm')
+connectToDatabase();
+
+let db = client.db('psm');
 
 export default db;
