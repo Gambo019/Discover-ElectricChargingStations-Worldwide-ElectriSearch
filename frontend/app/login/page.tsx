5de12d5a890cf4023e95d7faa5855fc94c60c6b2
G1119 <jasonzhong19941119@gmail.com>
2024-06-03 21:46:06 -0700

    Connecting to new Backned

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -16,7 +16,7 @@ function Login () {
 
   const handleLogin = async () => {
     // Make Post API call request to trigger backend login function
-    const response = await fetch('http://127.0.0.1:4000/auth/login', {
+    const response = await fetch('http://localhost:4000/auth/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
@@ -26,6 +26,10 @@ function Login () {
       console.log(password);
       
       console.log('It worked');
+      
+    }
+    else{
+      console.log(response);
       
     }
   }
